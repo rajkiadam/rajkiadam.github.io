1099e07b86bb12b920fe6e6fa7965db33e6e8418
rajkiadam <rajkiadam@gmail.com>
2023-08-05 21:49:27 +0200

    rotate

--- in-my-heart/heart.js
+++ in-my-heart/heart.js
@@ -221,7 +221,7 @@ let object;
     object.position.y = 0;
     object.position.z = 0;
 
-    object.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), 1.5707963268)
+    object.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), -1.5707963268)
    
     scene.add( object );
   
