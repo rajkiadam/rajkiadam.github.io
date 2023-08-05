8e9e199b20aaa79cada393c71f990a7d43f3e1f0
rajkiadam <rajkiadam@gmail.com>
2023-08-05 22:34:09 +0200

    size

--- in-my-heart/heart.js
+++ in-my-heart/heart.js
@@ -194,7 +194,7 @@ function setControls (camera, domElement, deviceOrientationMode) {
 }
 
 function createCylinder() {
-  const geometry = new THREE.CylinderGeometry(30, 30, 40, 64, 1, true);
+  const geometry = new THREE.CylinderGeometry(80, 80, 40, 64, 1, true);
   const materialOuter = new THREE.MeshBasicMaterial({
     map: new THREE.TextureLoader().load("https://threejs.org/examples/textures/758px-Canestra_di_frutta_(Caravaggio).jpg")
   });
